9ed723460a0f9ddc3b356c6d55dd72cd5dcb6a1e
Manuel Pérez <manuel.perez@alumnos.usm.cl>
2018-05-14 23:07:39 -0400

    Solución Accepted para el problema, 1001 - Extremely Basic

---
+++ Beginner/[AC] 1001 - Extremely Basic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+
+    int n1, n2;
+    int suma;
+
+    cin >> n1;
+    cin >> n2;
+
+    suma = n1 + n2;
+
+    cout << "X = " << suma << endl;
+
+return 0;
+}

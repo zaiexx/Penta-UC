8b48fcb782a7c03be4edd5ea0f53b7363c3010f6
Manuel Pérez <manuel.perez@alumnos.usm.cl>
2018-05-15 23:58:48 -0400

    Solución Accepted para el problema 1004 - Simple Product

---
+++ Beginner/[AC] 1004 - Simple Product.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+
+    long long n1, n2;
+
+    cin >> n1;
+    cin >> n2;
+
+    cout << "PROD = " << n1 * n2 << endl;
+
+
+return 0;
+}
